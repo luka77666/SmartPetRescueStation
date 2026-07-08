693cf728a43b0e4bffac58a6e0d8da70221f241e
luka77666 <luka157666@gmail.com>
2026-07-08 13:37:04 +0800

    Delete 电子222-202210214214-林湘龙-代码 directory

--- 电子222-202210214214-林湘龙-代码/智能宠物救助站电片机代码/SYSTEM/iwdg/iwdg.h
+++
@@ -1,9 +0,0 @@
-#ifndef __IWDG_H
-#define __IWDG_H
-
-#include "stm32f10x.h"
-
-void IWDG_Init(uint16_t prer, uint16_t rlr); // 独立看门狗初始化（参数：预分频值、重装载值）
-void IWDG_Feed(void); // 喂狗（重置看门狗计数器）
-
-#endif
